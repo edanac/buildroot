d5d3a88c2e48788480e44e885a568a1dd62f36dd
Rhys Eddy <rhyseddy@gmail.com>
2017-02-28 18:40:44 +1100

    Version bump for autobahn related scripts

--- package/python-autobahn/python-autobahn.mk
+++ package/python-autobahn/python-autobahn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AUTOBAHN_VERSION = 0.16.0
+PYTHON_AUTOBAHN_VERSION = 0.17.2
 PYTHON_AUTOBAHN_SOURCE = autobahn-$(PYTHON_AUTOBAHN_VERSION).tar.gz
-PYTHON_AUTOBAHN_SITE = https://pypi.python.org/packages/68/37/2523309412f0fb93a7784c65e5c5639b4b5d7a84f91374ad6c3c2d5af60b
+PYTHON_AUTOBAHN_SITE = https://pypi.python.org/packages/1b/c9/b4c8fa9aa92d5a5ea6786b173b1daf5ae57682aeed059987a74637d6e0fe
 PYTHON_AUTOBAHN_LICENSE = MIT
 PYTHON_AUTOBAHN_LICENSE_FILES = LICENSE
 PYTHON_AUTOBAHN_SETUP_TYPE = setuptools
--- package/python-txaio/python-txaio.mk
+++ package/python-txaio/python-txaio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TXAIO_VERSION = 2.5.1
+PYTHON_TXAIO_VERSION = 2.6.1
 PYTHON_TXAIO_SOURCE = txaio-$(PYTHON_TXAIO_VERSION).tar.gz
-PYTHON_TXAIO_SITE = https://pypi.python.org/packages/45/e1/f7d88767d65dbfc20d4b4aa0dad657dbbe8ca629ead2bef24da04630a12a
+PYTHON_TXAIO_SITE = https://pypi.python.org/packages/ef/8c/d7410b7509fe6510980c939453517567fa44f35357b8b23e6d346d08b7db
 PYTHON_TXAIO_LICENSE = MIT
 PYTHON_TXAIO_LICENSE_FILES = LICENSE
 PYTHON_TXAIO_SETUP_TYPE = setuptools
